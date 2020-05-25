55bafe346665038f4e68cf6f0b15b3e24d4abc4e
LauraM-hub <laura_mng@yahoo.com>
2020-05-25 09:52:07 +0300

    adding calculator

---
+++ calculator/script2.js
@@ -0,0 +1,85 @@
+let firstOperand;
+let secondOperand;
+let result;
+let operator;
+let decimal;
+
+
+function numericPress(value){
+    if(!result){
+        firstOperand = value;
+        result = "";
+    } else {
+        if (value ==="."){
+            if (decimal == false){
+                firstOperand += value;
+                decimal = true;
+            } 
+        } else {
+            firstOperand += value;
+        }    
+    
+    }
+
+    document.getElementById("entry").value = firstOperand;
+
+}
+
+
+function operatorPress(operatorInput){
+    if(!result){
+        secondOperand = firstOperand;
+    } else {
+        secondOperand = result;
+    }
+
+    firstOperand = "";
+    decimal = false;
+    operator = operatorInput;
+    result = "";
+    document.getElementById("entry").value = operator;
+
+}
+
+
+function equalPress(){
+
+    decimal = false;
+ 
+
+    secondOperand = parseFloat(secondOperand);
+    firstOperand = parseFloat(firstOperand);
+ 
+
+    switch(operator){
+        case "+":
+            result = secondOperand + firstOperand;
+            break;
+        case "-":
+            result = secondOperand - firstOperand;
+            break;
+        case "*":
+            result = secondOperand * firstOperand;
+            break;
+        case "/":
+            result = secondOperand / firstOperand;
+            break;
+        default:
+            result = firstOperand;
+    }
+ 
+    secondOperand = result;
+ 
+    document.getElementById("entry").value = result;
+}
+
+
+function clearPress(){
+    firstOperand = "";
+    secondOperand = "";
+    result = "";
+    operator = "";
+    decimal = "";
+
+    document.getElementById("entry").value = "0";
+}
